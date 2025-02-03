9243906eb45898e251ccedf9b77d52891cb1ffdb
Dolly Chang (張佩雯) <dollychang@qnap.com>
2025-02-03 16:19:17 +0800

    test

--- javascripts/discourse/initializers/activeUserLoggedInStatus.js
+++ javascripts/discourse/initializers/activeUserLoggedInStatus.js
@@ -4,7 +4,7 @@ import { userLoggedInStatus } from "../utilis";
 export default {
     name: "activeUserLoggedInStatus",
     initialize() {
-        withPluginApi("0.8.18", (api) => {
+        withPluginApi("0.8.7", (api) => {
             api.onPageChange(() => {
                 userLoggedInStatus(api);
                 console.log("open Page changed");
