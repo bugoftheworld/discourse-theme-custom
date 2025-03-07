28b3fa1c1d5f222858db49ccf5ccc9ff951b3f71
Dolly Chang (張佩雯) <dollychang@qnap.com>
2025-03-07 10:50:43 +0800

    test

--- javascripts/discourse/initializers/activeUserLoggedInStatus.js
+++ javascripts/discourse/initializers/activeUserLoggedInStatus.js
@@ -6,18 +6,18 @@ export default {
     name: "activeUserLoggedInStatus",
     initialize() {
         withPluginApi("0.8.18", (api) => {
-            //get isHomepage from defaultHomepage
             const isHomepage = () => {
                 const { currentRouteName } = api.router;
                 return currentRouteName === `discovery.${defaultHomepage()}`;
             };
-            console.log("testing activeUserLoggedInStatus--", isHomepage);
+
+            console.log("testing activeUserLoggedInStatus--", isHomepage());
             api.onPageChange(() => {
-                userLoggedInStatus(api, isHomepage);
+                userLoggedInStatus(api, isHomepage());
                 // console.log("testing Page changed");
             });
             api.onAppEvent("post-stream:posted", () => {
-                userLoggedInStatus(api, isHomepage);
+                userLoggedInStatus(api, isHomepage());
                 // console.log("testing posted");
             });
         });
