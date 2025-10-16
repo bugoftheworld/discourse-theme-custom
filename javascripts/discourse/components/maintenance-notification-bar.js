cbbc86a4b2ba8b0a556ddfb9e8148cfbfbcd3535
Dolly Chang (張佩雯) <dollychang@qnap.com>
2025-10-16 11:16:42 +0800

    adde test

--- javascripts/discourse/components/maintenance-notification-bar.js
+++ javascripts/discourse/components/maintenance-notification-bar.js
@@ -7,9 +7,12 @@ export default class MaintenanceNotificationBar extends Component {
         const endTime = parseInt(maintenanceNotificationBar.getAttribute('data-end-time'), 10);
         const durationTime = parseInt(maintenanceNotificationBar.getAttribute('data-duration-time'), 10);
         const currentTime = new Date().getTime();
+        console.log('Current Time:', currentTime);
         if (currentTime > endTime) {
+            console.log('Maintenance period has ended. Hiding notification bar.');
             maintenanceNotificationBar.style.display = 'none';
         } else {
+            console.log('Maintenance period is ongoing. Showing notification bar.');
             setTimeout(() => {
                 maintenanceNotificationBar.style.transition = 'opacity .3s';
                 maintenanceNotificationBar.style.opacity = '0';
